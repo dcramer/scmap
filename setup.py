ee11febebe1389627ea205d1133ddaca3820682b
David Cramer <dcramer@gmail.com>
2010-07-27 19:20:16 -0700

    Setup packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='scmap',
+    version='0.1',
+    description='',
+    author='Forrest Voight',
+    url='http://github.com/dcramer/scmap',
+    packages=find_packages(),
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)
